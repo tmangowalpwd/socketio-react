da53ff9ded352376b141a91c0fa1fe1e75a0d6ab
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-04-07 15:48:00 +0700

    send message

--- src/pages/chat-room.jsx
+++ src/pages/chat-room.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
-import { Container } from "reactstrap";
+import { Button, Container, Input } from "reactstrap";
 import { io } from "socket.io-client";
 
 const socketConnection = io("http://localhost:2000");
 
 const ChatRoom = () => {
+  const [messages, setMessages] = useState([]);
+
+  const [inputMessage, setInputMessage] = useState("");
+
   const params = useParams();
 
   const joinRoom = async () => {
@@ -22,20 +26,62 @@ const ChatRoom = () => {
     }
   };
 
+  const fetchMessages = async () => {
+    try {
+      const roomId = params.roomId;
+      const messageRes = await axios.get(
+        `http://localhost:2000/chat/room/${roomId}/messages`
+      );
+
+      setMessages(messageRes.data.result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     if (localStorage.getItem("user")) {
-      console.log("test");
       joinRoom();
+      fetchMessages();
+
+      socketConnection.on("NEW_MESSAGE", (newMessage) => {
+        setMessages([...messages, newMessage]);
+      });
     }
+
+    return () => {
+      socketConnection.disconnect();
+    };
   }, []);
+
+  const sendMessageHandler = async () => {
+    try {
+      const roomId = params.roomId;
+      const userId = JSON.parse(localStorage.getItem("user")).id;
+
+      await axios.post(`http://localhost:2000/chat/message/room/${roomId}`, {
+        user_id: userId,
+        message: inputMessage,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   if (!localStorage.getItem("user")) {
     return <Navigate to="/rooms" />;
   }
 
   return (
-    <Container>
+    <Container className="py-5">
       <h2>Chat Room</h2>
+      <div className="d-flex">
+        <Input
+          onChange={(e) => setInputMessage(e.target.value)}
+          className="me-5"
+        />
+        <Button onClick={sendMessageHandler}>Send</Button>
+      </div>
     </Container>
   );
 };
